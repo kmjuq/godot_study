33030f5797fc6623f6f998816ba25204bd2bed1d
柯梦剑 <mengjian.ke@hand-china.com>
2022-01-28 19:58:16 +0800

    adjust

---
+++ scenes/study/script_class/ScriptClass.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# 创建一个脚本类，可以实例化
+class_name ScriptClass,"res://assets/study/script_class/buddy_orange.png"
+
+func _ready():
+	pass 
+	
+func class_instance_test():
+	print("class_instance_test")
+	pass
